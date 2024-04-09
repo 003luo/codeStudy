66ea707fa666a432b7b0f13f6f2d05903a3b70b0
003luo <2723422761@qq.com>
2024-04-09 20:38:03 +0800

    添加点赞功能

--- src/main/java/com/codestudy/controller/LikesController.java
+++ src/main/java/com/codestudy/controller/LikesController.java
@@ -1,10 +1,11 @@
 package com.codestudy.controller;
 
 import com.codestudy.entity.Likes;
-import com.codestudy.entity.Result;
+import com.codestudy.entity.Result1;
 import com.codestudy.service.LikesService;
 import com.google.common.base.Preconditions;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,22 +22,20 @@ public class LikesController {
 
     /**
      * 用户点赞
-     *
-     * @param
-     * @param
      */
+    @ApiOperation(value = "点赞")
     @PostMapping("/dolike")
-    public Result dolikes(@RequestBody Likes likes) {
+    public Result1 dolikes(@RequestBody Likes likes) {
         try {
             // 成功的逻辑
             Preconditions.checkNotNull(likes.getNoteId(), "笔记Id不能为空");
             Boolean res = likesService.dolikes(likes.getNoteId());
-            if (res) return Result.ok("操作成功！");
-            return Result.fail("操作失败！");
+            if (res) return Result1.success("操作成功！");
+            return Result1.error("操作失败！");
         } catch (Exception e) {
             // 业务层如果有抛异常，那么走这里
             log.error("likesController.dolikes.error:{}", e.getMessage(), e);
-            return Result.fail(e.getMessage());
+            return Result1.error(e.getMessage());
         }
     }
 }
